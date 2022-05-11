c65caf8fec43a011a3b76f131406841cdc334336
MusicDev33 <smccowan33@gmail.com>
2022-05-11 15:42:41 -0700

    add eggs

--- src/data/rwl.js
+++ src/data/rwl.js
@@ -7,7 +7,7 @@ const pod2d = ['cooked porkchop', 'cooked chicken', 'steak', 'pumpkin pie', 'bak
 const pod3u = ['azalea', 'flowering azalea', 'moss block', 'moss carpet', 'vines'];
 const pod3d = ['potato', 'carrot', 'apple', 'melon slice', 'wheat'];
 
-const pod4u = ['cactus'];
+const pod4u = ['cactus', 'egg'];
 const pod4d = ['melon', 'pumpkin', 'wheat seeds', 'pumpkin seeds', 'melon seeds'];
 
 const pod5u = ['oak door', 'acacia door', 'jungle door', 'crimson door', 'warped door'];
